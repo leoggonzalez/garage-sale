908619564a141ec862c16c7f1d5a7307afa6d983
leoggonzalez <leoggonzalez@hotmail.com>
2020-12-27 16:51:54 +0100

    added more colors

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,6 @@
 module WelcomeHelper
   def random_card_color
-    colors = ['blue-light', 'cyan', 'green', 'green-light', 'yellow']
-    colors[rand(5)]
+    colors = ['blue-light', 'cyan', 'green', 'green-light', 'yellow', 'pink', 'beige', 'salmon', 'gray', 'purple']
+    colors[rand(colors.length)]
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+require("typeface-nunito");
+require("typeface-poppins");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -40,7 +42,7 @@ document.addEventListener("turbolinks:load", function () {
     nextArrow.addEventListener("click", () => {
       const activeSlide = slider.querySelector(".slider__slide.active");
       if (activeSlide) {
-        const number = activeSlide.dataset.number;
+        const number = Number(activeSlide.dataset.number);
         let nextSlide = null;
         if (number < slides.length - 1) {
           nextSlide = slides.item(number + 1);
@@ -56,7 +58,7 @@ document.addEventListener("turbolinks:load", function () {
     previousArrow.addEventListener("click", () => {
       const activeSlide = slider.querySelector(".slider__slide.active");
       if (activeSlide) {
-        const number = activeSlide.dataset.number;
+        const number = Number(activeSlide.dataset.number);
         let nextSlide = null;
         if (number > 0) {
           nextSlide = slides.item(number - 1);
