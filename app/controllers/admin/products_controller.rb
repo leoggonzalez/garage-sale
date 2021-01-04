ca07ef16182fbf551bf4a79d133320026ee24239
leoggonzalez <leoggonzalez@hotmail.com>
2021-01-04 18:18:26 +0100

    fixed more links

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -30,7 +30,7 @@ module Admin
 
       respond_to do |format|
         if @product.save
-          format.html { redirect_to @product, notice: 'Product was successfully created.' }
+          format.html { redirect_to admin_product_path(@product), notice: 'Product was successfully created.' }
           format.json { render :show, status: :created, location: @product }
         else
           format.html { render :new }
@@ -44,7 +44,7 @@ module Admin
     def update
       respond_to do |format|
         if @product.update(product_params)
-          format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+          format.html { redirect_to admin_product_path(@product), notice: 'Product was successfully updated.' }
           format.json { render :show, status: :ok, location: @product }
         else
           format.html { render :edit }
@@ -58,20 +58,20 @@ module Admin
     def destroy
       @product.destroy
       respond_to do |format|
-        format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+        format.html { redirect_to admin_products_url, notice: 'Product was successfully destroyed.' }
         format.json { head :no_content }
       end
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_product
-        @product = Product.find(params[:id])
-      end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
-      # Only allow a list of trusted parameters through.
-      def product_params
-        params.require(:product).permit(:name, :description, :url, :price)
-      end
+    # Only allow a list of trusted parameters through.
+    def product_params
+      params.require(:product).permit(:name, :description, :url, :price)
+    end
   end
 end
